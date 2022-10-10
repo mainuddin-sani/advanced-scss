40c5ab77ab180726ebd28439e966c09a6868173f
Mainuddin Sani <main12sani@gmail.com>
2022-10-11 02:02:17 +0600

    start devider & end devider find

--- src/app.js
+++ src/app.js
@@ -1,1 +1,70 @@
 // write your code
+
+import database from "./json/exam-databse.json";
+
+function findId(prop, type) {
+  const orginalId = prop.split(type)[1];
+  if (type === "__") {
+    return {
+      id: orginalId,
+      start: prop,
+      end: "_#" + orginalId,
+    };
+  } else {
+    return {
+      id: orginalId,
+      end: prop,
+      start: "__" + orginalId,
+    };
+  }
+}
+
+function isString(prop) {
+  return typeof prop === "string";
+}
+
+function isStartDevider(prop) {
+  let result = false;
+  for (let obj in prop) {
+    if (isString(obj) && obj.startsWith("__")) {
+      const { id, end } = findId(obj, "__");
+      if (Reflect.has(prop, end) && !Reflect.has(prop, id)) {
+        result = true;
+      } else {
+        console.warn("Someting is wrong " + obj);
+      }
+    }
+  }
+  return result;
+}
+
+function isEndDevider(prop) {
+  let result = false;
+  for (let obj in prop) {
+    if (isString(obj) && obj.startsWith("_#")) {
+      const { id, start } = findId(obj, "_#");
+      if (Reflect.has(prop, start) && !Reflect.has(prop, id)) {
+        result = true;
+      } else {
+        console.warn("Someting is wrong " + obj);
+      }
+    }
+  }
+  return result;
+}
+
+let queue = [];
+
+for (let property of database) {
+  const isStart = isStartDevider(property);
+  const isEnd = isEndDevider(property);
+  console.log(isStart);
+
+  for (let isStartProperty in property) {
+    if(isStart){
+        const {id} = findId(isStartProperty, "__");
+        console.log(id);
+    }
+  }
+}
+console.log(queue);
